b4fd4b022aae51a5d519dd7c4eba61df1c0fa000
Raphaël Payet <raphpay.dev@gmail.com>
2023-10-05 11:27:43 +0200

    Refactor: Implement the new background image to SignUp screen #2

--- src/ui/components/TopBarNav.js
+++ src/ui/components/TopBarNav.js
@@ -7,21 +7,22 @@ function TopBarNav(props) {
 
   return (
     <View style={styles.container}>
-      {leftComponent}
+      {leftComponent ?? <View style={styles.emptyView} />}
       <Image 
         source={require('../assets/images/logo/Horizontal-Logo.png')}
         resizeMode={'contain'}
         style={styles.logo}
       />
-      {rightComponent}
+      {rightComponent ?? <View style={styles.emptyView}/>}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     width: '100%',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     height: 54,
   },
@@ -29,6 +30,9 @@ const styles = StyleSheet.create({
     height: 54,
     width: 200,
   },
+  emptyView: {
+    flex: 0.33
+  }
 })
 
 export default TopBarNav;
